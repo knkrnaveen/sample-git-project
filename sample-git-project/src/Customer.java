61cb0e347263a0926d73cc75f7c84bff33007735
knkrnaveen <reddynaveen.k98@gmail.com>
2021-10-19 18:34:48 +0530

    Refresh

---
+++ sample-git-project/src/Customer.java
@@ -0,0 +1,17 @@
+
+public class Customer {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+	    int salary;
+	    String name;
+	    String Country;
+	    int age;
+	    String Gender;
+	    String Level;
+	    //changed project name
+
+	}
+
+}
